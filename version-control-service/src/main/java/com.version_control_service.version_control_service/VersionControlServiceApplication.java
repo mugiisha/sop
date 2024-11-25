9902790d4cc012b3564980edf676df423486a3c7
mugishaelvis456@gmail.com <mugishaelvis456@gmail.com>
2024-11-25 07:56:24 +0200

    working on version sop functionalities

--- version-control-service/src/main/java/com.version_control_service.version_control_service/VersionControlServiceApplication.java
+++ version-control-service/src/main/java/com.version_control_service.version_control_service/VersionControlServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VersionControlServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VersionControlServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VersionControlServiceApplication.class, args);
+    }
 
 }
