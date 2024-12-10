5ab89dae408a56e77bbb8ce1c8cb6b1f0d47273b
virgile-am <ndayambaje.virgile@amalitechtraining.org>
2024-12-10 21:11:30 +0200

    fix deployment

--- version-control-service/src/main/java/com.version_control_service.version_control_service/dto/SopDto.java
+++ version-control-service/src/main/java/com.version_control_service.version_control_service/dto/SopDto.java
@@ -1,144 +1,144 @@
-//package com.version_control_service.version_control_service.dto;
-//
-//
-//import java.util.List;
-//
-//public class SopDto {
-//    private String id;
-//    private String title;
-//    private String description;
-//
-//    public String getDocumentUrl() {
-//        return documentUrl;
-//    }
-//
-//    public void setDocumentUrl(String documentUrl) {
-//        this.documentUrl = documentUrl;
-//    }
-//
-//    public String getImageUrl() {
-//        return imageUrl;
-//    }
-//
-//    public void setImageUrl(String imageUrl) {
-//        this.imageUrl = imageUrl;
-//    }
-//
-//    public String getVersion() {
-//        return version;
-//    }
-//
-//    public String getCode() {
-//        return code;
-//    }
-//
-//    public void setVisibility(String visibility) {
-//        this.visibility = visibility;
-//    }
-//
-//    public String getVisibility() {
-//        return visibility;
-//    }
-//
-//    public void setCategory(String category) {
-//        this.category = category;
-//    }
-//
-//    public String getCategory() {
-//        return category;
-//    }
-//
-//    public void setCode(String code) {
-//        this.code = code;
-//    }
-//
-//
-//    public void setVersion(String version) {
-//        this.version = version;
-//    }
-//
-//    public String getStatus() {
-//        return status;
-//    }
-//
-//    public void setStatus(String status) {
-//        this.status = status;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public List<String> getReviewers() {
-//        return reviewers;
-//    }
-//
-//    public void setReviewers(List<String> reviewers) {
-//        this.reviewers = reviewers;
-//    }
-//
-//    public List<String> getAuthors() {
-//        return authors;
-//    }
-//
-//    public void setAuthors(List<String> authors) {
-//        this.authors = authors;
-//    }
-//
-//    public List<String> getApprovers() {
-//        return approvers;
-//    }
-//
-//    public void setApprovers(List<String> approvers) {
-//        this.approvers = approvers;
-//    }
-//
-//
-//    private String status;
-//    private String version;
-//    private String imageUrl;
-//    private String documentUrl;
-//    private String code;
-//    private String visibility;
-//    private String category;
-//    private List<String> reviewers;
-//    private List<String> authors;
-//    private List<String> approvers;
-//
-//    public SopDto(String id, String title, String description, String status, String version, String imageUrl, String documentUrl, String code, String visibility , String category , List<String> reviewers, List<String> authors, List<String> approvers) {
-//        this.id = id;
-//        this.title = title;
-//        this.description = description;
-//        this.status = status;
-//        this.version = version;
-//        this.imageUrl = imageUrl;
-//        this.documentUrl = documentUrl;
-//        this.code = code;
-//        this.visibility = visibility;
-//        this.category = category;
-//        this.reviewers = reviewers;
-//        this.authors = authors;
-//        this.approvers = approvers;
-//    }
-//
-//}
+package com.version_control_service.version_control_service.dto;
+
+
+import java.util.List;
+
+public class SopDto {
+    private String id;
+    private String title;
+    private String description;
+
+    public String getDocumentUrl() {
+        return documentUrl;
+    }
+
+    public void setDocumentUrl(String documentUrl) {
+        this.documentUrl = documentUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<String> getReviewers() {
+        return reviewers;
+    }
+
+    public void setReviewers(List<String> reviewers) {
+        this.reviewers = reviewers;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors) {
+        this.authors = authors;
+    }
+
+    public List<String> getApprovers() {
+        return approvers;
+    }
+
+    public void setApprovers(List<String> approvers) {
+        this.approvers = approvers;
+    }
+
+
+    private String status;
+    private String version;
+    private String imageUrl;
+    private String documentUrl;
+    private String code;
+    private String visibility;
+    private String category;
+    private List<String> reviewers;
+    private List<String> authors;
+    private List<String> approvers;
+
+    public SopDto(String id, String title, String description, String status, String version, String imageUrl, String documentUrl, String code, String visibility , String category , List<String> reviewers, List<String> authors, List<String> approvers) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.version = version;
+        this.imageUrl = imageUrl;
+        this.documentUrl = documentUrl;
+        this.code = code;
+        this.visibility = visibility;
+        this.category = category;
+        this.reviewers = reviewers;
+        this.authors = authors;
+        this.approvers = approvers;
+    }
+
+}
