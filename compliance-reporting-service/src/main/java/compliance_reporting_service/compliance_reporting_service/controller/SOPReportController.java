43c7e40b4688b4d7128d1ce9de89f9b3af7e1f96
unknown <you@example.com>
2025-01-06 09:52:24 -0800

    fix deployment

--- compliance-reporting-service/src/main/java/compliance_reporting_service/compliance_reporting_service/controller/SOPReportController.java
+++ compliance-reporting-service/src/main/java/compliance_reporting_service/compliance_reporting_service/controller/SOPReportController.java
@@ -1,18 +1,11 @@
 package compliance_reporting_service.compliance_reporting_service.controller;
 
 import compliance_reporting_service.compliance_reporting_service.dto.ReportResponseDto;
-import compliance_reporting_service.compliance_reporting_service.model.SOPReport;
 import compliance_reporting_service.compliance_reporting_service.service.SOPReportService;
 import compliance_reporting_service.compliance_reporting_service.util.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reports")
